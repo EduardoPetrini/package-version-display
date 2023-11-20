c690191e76264961d1be72ae4042266104bea173
EduardoPetrini <eduardo.petrini@outlook.com>
2023-11-20 09:53:43 -0300

    feat: first commit - display the package.json version

---
+++ README.md
@@ -0,0 +1,19 @@
+# The Package Version Display - VSCode Extension
+This extension make it easy to see the version of your current working no JavaScript/TypeScript module.
+
+## Installation
+
+Download it from the [VSCode Extension Marketplace]()
+
+## How does it work?
+This is a package.json file example:
+
+![Your package.json](docs/images/package-json.png)
+
+Here is how the extension displays the version on VSCode's status bar:
+![Your package.json](docs/images/display-version.png)
+
+## Requirements
+
+Currently it works only for JavaScript/TypeScript modules.
+They must contain the package.json file.
---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBarItem.text = 'No Version';
+  statusBarItem.show();
+
+  // statusBarItem.tooltip
+  context.subscriptions.push(statusBarItem);
+
+  const version = await getPackageVersion();
+
+  if (!version) {
+    return;
+  }
+
+  let displayVersion = version.trim();
+
+  if (!displayVersion.startsWith('v')) {
+    displayVersion = `v${version}`;
+  }
+
+  statusBarItem.text = displayVersion;
+}
+
+async function getPackageVersion() {
+  const workspace = vscode.workspace.workspaceFolders?.[0].uri;
+  if (!workspace) {
+    return '';
+  }
+
+  const packageJsonPath = vscode.Uri.joinPath(workspace, 'package.json');
+
+  try {
+    const byteArray = await vscode.workspace.fs.readFile(packageJsonPath);
+    const content = new TextDecoder().decode(byteArray);
+
+    const packageJson = JSON.parse(content);
+
+    return packageJson?.version;
+  } catch (error) {
+    return '';
+  }
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
